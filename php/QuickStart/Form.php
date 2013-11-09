9cfbad637a318bd57152aca6947c0782be7dcfad
Doug Wollison <doug@wollison.net>
2013-11-09 12:02:43 -0500

    Basics of Form kit; build_tag method and empty build_field(s) methods.

--- php/QuickStart/Form.php
+++ php/QuickStart/Form.php
@@ -10,4 +10,53 @@ namespace QuickStart;
  */
 
 class Form{
+	public static $accepted_attrs = array( 'accesskey', 'autocomplete', 'checked', 'class', 'cols', 'disabled', 'id', 'max', 'maxlength', 'min', 'multiple', 'name', 'placeholder', 'readonly', 'required', 'rows', 'size', 'style', 'tabindex', 'title', 'type', 'value' );
+
+	/**
+	 * Build an HTML tag.
+	 *
+	 * @since 1.0
+	 *
+	 * @param string $tag     The tag name.
+	 * @param array  $atts    The tag attributes.
+	 * @param string $content The tag content.
+	 *
+	 * @return string The html of the tag.
+	 */
+	public static function build_tag( $tag, $atts, $content = null, $accepted = null ) {
+		if ( is_null( $accepted ) ) {
+			$accepted = self::$accepted_attrs;
+		}
+	
+		$html = "<$tag";
+
+		foreach ( $atts as $attr => $value ) {
+			if ( is_numeric ( $attr ) ) {
+				$html .= " $value";
+			} else {
+				// Make sure it's a registerd attribute (or data- attribute)
+				if ( ! in_array( $attr, $accepted ) && strpos( $attr, 'data-' ) !== 0 ) continue;
+
+				if ( is_array( $value ) ) {
+					// Implode into a space separated list
+					$value = implode( ' ', $value );
+				}
+				$html .= " $attr=\"$value\"";
+			}
+		}
+
+		if ( is_null( $content ) ) {
+			$html .= '/>';
+		} else {
+			$html .= ">$content</$tag>";
+		}
+
+		return $html;
+	}
+	
+	public static function build_field( $field, $settings = array(), $post = null ) {
+	}
+	
+	public static function build_fields($fields, $post = null, $echo = false){
+	}
 }
