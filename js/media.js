5354a6eee5d7954462fe6b45b990f09244f74a99
Doug Wollison <doug@ninesides.com>
2013-11-11 16:36:26 -0500

    Added preload utility; code cleanup.

--- js/media.js
+++ js/media.js
@@ -17,11 +17,11 @@ window.QS = window.QS || {};
 		 *
 		 * @return object The first attachment selected
 		 */
-		attachment: function(frame){
-			if(frame == undefined)
+		attachment: function( frame ) {
+			if ( frame == undefined )
 				frame = this.frame;
 
-			var attachment = frame.state().get('selection').first();
+			var attachment = frame.state().get( 'selection' ).first();
 
 			return attachment.attributes;
 		},
@@ -33,26 +33,58 @@ window.QS = window.QS || {};
 		 *
 		 * @return array An array of attachments
 		 */
-		attachments: function(frame){
-			if(frame == undefined)
+		attachments: function( frame ) {
+			if ( frame == undefined )
 				frame = this.frame;
 
 			var attachments = [];
 
 			var collection;
 
-			if(frame.options.state == 'gallery-edit'){
-				collection = frame.states.get('gallery-edit').get('library');
-			}else{
-				collection = frame.state().get('selection');
+			if ( frame.options.state == 'gallery-edit' ) {
+				collection = frame.states.get( 'gallery-edit' ).get( 'library' );
+			} else {
+				collection = frame.state().get( 'selection' );
 			}
 
-			collection.map(function(item, i, items){
-				attachments.push(item.attributes);
-			});
+			collection.map( function( item, i, items ) {
+				attachments.push( item.attributes );
+			} );
 
 			return attachments;
 		}
+
+		/**
+		 * Preload the media manager with provided attachment ids
+		 *
+		 * @param string|array ids   A comma separated string or array of ids
+		 * @param wp.media     frame The frame workflow (defaults to current frame stored in QS.media)
+		 */
+		preload: function( ids, frame ) {
+			if ( frame == undefined )
+				frame = this.frame;
+
+			if ( ids != undefined ) {
+				var selection = frame.state().get('selection');
+				var attachment;
+
+				selection.reset( [] );
+
+				if ( 'string' == typeof ids ) {
+					ids = ids.split( ',' );
+				}
+
+				var id;
+				for ( var i in ids ) {
+					id = ids[ i ];
+
+					attachment = wp.media.attachment( id );
+					attachment.fetch();
+
+					selection.add( attachment ? [ attachment ] : [] );
+				}
+			}
+		},
 	});
 
 	/**
