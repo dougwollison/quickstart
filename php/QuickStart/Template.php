842ac07d1ac25248ba03303625973a8c366684c3
Doug Wollison <doug@wollison.net>
2013-11-15 23:46:03 -0500

    Added doc start method.

--- php/QuickStart/Template.php
+++ php/QuickStart/Template.php
@@ -10,4 +10,30 @@ namespace QuickStart;
  */
 
 class Template{
+	/**
+	 * Print out the start of the document.
+	 *
+	 * Doctype and opening html tag with
+	 * IE conditional comments for classes.
+	 *
+	 * @since 1.0.0
+	 */
+	public static function doc_start() {
+		?><!DOCTYPE html>
+<!--[if lte IE 6]>
+<html id="ie6" class="ie8- ie7- ie6-" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7" class="ie8- ie7-" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8" class="ie8-" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 9]>
+<html id="ie8" class="ie8-" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 6) | !(IE 7) | !(IE 8) | !(IE 9)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]--><?php
+	}
 }
