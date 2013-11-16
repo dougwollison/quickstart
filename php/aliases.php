eef803a5f3b3cf815e7911f40abcaf82a0aacc96
Doug Wollison <doug@wollison.net>
2013-11-16 15:35:48 -0500

    Revised QuickStart alias to use true as default for $global

--- php/aliases.php
+++ php/aliases.php
@@ -14,10 +14,10 @@
  *
  * @param array	$configs	An associative array of theme configuration options
  * @param array	$default	Optional an associative array of default values to use
- * @param bool	$global		Wether or not to assign this new instance to the $QuickStart global variable
+ * @param bool	$global		Optional Wether or not to assign this new instance to the $QuickStart global variable
  * @return \QuickStart $QS The class instance
  */
-function QuickStart( $configs, $defaults = array(), $global = false ) {
+function QuickStart( $configs, $defaults = array(), $global = true ) {
 	$obj = new QuickStart\Setup( $configs, $defaults );
 
 	if ( $global ) {
