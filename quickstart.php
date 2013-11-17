a0fb6fe3bc15fd5e44e00ed245b6f1ff8b045bd8
Doug Wollison <doug@wollison.net>
2013-11-16 22:58:55 -0500

    Fixed version number

--- quickstart.php
+++ quickstart.php
@@ -3,7 +3,7 @@
 Plugin Name: QuickStart
 Plugin URI: https://github.com/dougwollison/quickstart
 Description: A utility kit for quick development of WordPress themes (and plugins). ***YOUR CUSTOM THEME RELIES ON THIS PLUGIN***
-Version: 1.0
+Version: 1.0.0
 Author: Doug Wollison
 Author URI: http://dougw.me
 License: GPL2
