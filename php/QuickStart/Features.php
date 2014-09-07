1a896aa740b509fe6e3a17c1b4c25f8c3ceb8f49
Doug Wollison <doug@wollison.net>
2014-09-06 23:32:54 -0400

    Undid reorganizing; Feature related hooks are now right in Features.

--- php/QuickStart/Features.php
+++ php/QuickStart/Features.php
@@ -9,7 +9,20 @@ namespace QuickStart;
  * @since 1.0.0
  */
 
-class Features {
+class FeatureHooks extends \SmartPlugin {
+	/**
+	 * A list of internal methods and their hooks configurations.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 * @var array
+	 */
+	protected static $static_method_hooks = array(
+		'index_page_query'      => array( 'parse_query', 10, 1 ),
+		'index_page_title_part' => array( 'wp_title_parts', 10, 1 ),
+		'index_page_title'      => array( 'wp_title', 10, 1 ),
+	);
+	
 	// =========================
 	// !Order Manager
 	// =========================
@@ -255,17 +268,88 @@ class Features {
 				), 'default', 'reading' );
 			} else {
 				// Add the query/title hooks on the frontend
-				FeatureHooks::index_page_query( $post_type );
+				self::index_page_query( $post_type );
 				
 				// Call the appropriate title hook
 				if ( version_compare( get_bloginfo( 'version' ), '4.0', '>=' ) ) {
 					// Use new wp_title_parts filter method
-					FeatureHooks::index_page_title_part( $post_type );
+					self::index_page_title_part( $post_type );
 				} else {
 					// Use old wp_title filter method
-					FeatureHooks::index_page_title( $post_type );
+					self::index_page_title( $post_type );
 				}
 			}
 		}
+	}
+	
+	/**
+	 * Check if the page is a custom post type's index page.
+	 *
+	 * @since 1.6.0
+	 *
+	 * @param WP_Query $query     The query object (skip when saving).
+	 * @param string   $post_type The post type to check for.
+	 */
+	protected function _index_page_query( $query, $post_type ) {
+		$qv =& $query->query_vars;
+		
+		if ( '' != $qv['pagename'] ) {
+			$index = get_option( "page_for_{$post_type}_posts" );
+			if ( $query->queried_object_id == $index ) {
+				$post_type_obj = get_post_type_object( $post_type );
+				if ( ! empty( $post_type_obj->has_archive ) ) {
+					$qv['post_type'] = $post_type;
+					$qv['name'] = '';
+					$qv['pagename'] = '';
+					$query->is_page = false;
+					$query->is_singular = false;
+					$query->is_archive = true;
+					$query->is_post_type_archive = true;
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Change the first part of the title to display the index page's title.
+	 *
+	 * @since 1.6.0
+	 *
+	 * @param string $title_array The parts of the page title (skip when saving).
+	 * @param string $post_type   The post type to check for.
+	 */
+	protected function _index_page_title_part( $title_array, $post_type ) {
+		$index = get_option( "page_for_{$post_type}_posts" );
+		
+		// Check if this is the right post type archive and an index page is set
+		if ( is_post_type_archive() && get_query_var( 'post_type' ) == $post_type && $index ) {
+			$title_array[0] = get_the_title( $index );
+		}
+		
+		return $title_array;
+	}
+	
+	/**
+	 * Modify the title to display the index page's title.
+	 *
+	 * @since 1.6.0
+	 *
+	 * @deprecated 1.6.0 Exists solely for WordPress 3.9 and below.
+	 *
+	 * @param string $title       The page title (skip when saving).
+	 * @param string $post_type   The post type to check for.
+	 */
+	protected function _index_page_title( $title, $post_type ) {
+		$index = get_option( "page_for_{$post_type}_posts" );
+		
+		// Check if this is the right post type archive and an index page is set
+		if ( is_post_type_archive() && get_query_var( 'post_type' ) == $post_type && $index ) {
+			// Replace the archive title for the post type with the index page's title.
+			$archive_title = post_type_archive_title( '', false );
+			$page_title = get_the_title( $index );
+			$title = str_replace( $archive_title, $page_title, $title );
+		}
+		
+		return $title;
 	}
 }
