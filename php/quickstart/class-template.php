4e636701fc79573801936a2b09ae3972f3a39d4c
Doug Wollison <Doug@Dougs-iMac.lan>
2014-11-10 10:53:13 -0500

    Added google analytics code method

--- php/quickstart/class-template.php
+++ php/quickstart/class-template.php
@@ -16,7 +16,7 @@ class Template {
 	 * Doctype and opening html tag with
 	 * IE conditional comments for classes.
 	 *
-	 * @sicne 1.1.0 Fixed IE9 tagging.
+	 * @since 1.1.0 Fixed IE9 tagging.
 	 * @since 1.0.0
 	 */
 	public static function doc_start() {
@@ -37,5 +37,48 @@ class Template {
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
 <?php
+	}
+
+	/**
+	 * Print out the Google Analytics gode.
+	 *
+	 * @since 1.6.2
+	 *
+	 * @param string $account    The ID code of the account to track for.
+	 * @param string $production Optional A host name or IP address to check for before printing.
+	 */
+	public static function ga_code( $account, $production = null ) {
+		if ( ! is_null( $production ) ) {
+			// By default, check for the host name
+			$field = 'SERVER_NAME';
+
+			// If it looks like an IP address, check for server address
+			if ( preg_match( '/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/', $production ) ){
+				$field = 'SERVER_ADDR';
+			}
+
+			// If the check fails, don't print anything
+			if ( $_SERVER[ $field ] != $production ) {
+				return;
+			}
+		}
+
+		?>
+		<!-- Start Google Analytics tracking code -->
+		<script type="text/javascript">
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', '<?php echo $account; ?>']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+
+		</script>
+		<!-- End Google Analytics tracking code -->
+		<?php
 	}
 }
