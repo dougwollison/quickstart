7eb97f2c47b00005509af1854b1ec3298c94431d
Doug Wollison <doug@wollison.net>
2014-11-16 13:56:30 -0500

    Added aliases for Template and select Hooks/Tools methods.

--- php/aliases.php
+++ php/aliases.php
@@ -28,3 +28,87 @@ function QuickStart( $configs, $defaults = array(), $global = true ) {
 
 	return $obj;
 }
+
+// =========================
+// !Template aliases
+// =========================
+
+/**
+ * Alias to the doc_start template tag
+ *
+ * @see QuickStart\Template::doc_start()
+ */
+function qs_doc_start() {
+	QuickStart\Template::doc_start();
+}
+
+/**
+ * Alias to the ga_code template tag
+ *
+ * @see QuickStart\Template::ga_code()
+ */
+function qs_ga_code() {
+	QuickStart\Template::ga_code();
+}
+
+// =========================
+// !Tools aliases
+// =========================
+
+/**
+ * Alias to the upload tool
+ *
+ * @see QuickStart\Tools::upload()
+ */
+function qs_upload( $file, $attachment = array() ) {
+	QuickStart\Tools::upload( $file, $attachment );
+}
+
+/**
+ * Alias to the save_post_check tool
+ *
+ * @see QuickStart\Tools::save_post_check()
+ */
+function qs_save_post_check( $post_id, $post_type = null, $nonce_name = null, $nonce_value = null) ) {
+	QuickStart\Tools::save_post_check( $post_id, $post_type, $nonce_name, $nonce_value );
+}
+
+/**
+ * Alias to the simple_shortcode tool
+ *
+ * @see QuickStart\Tools::simple_shortcode()
+ */
+function qs_simple_shortcode( $atts, $content, $tag  ) {
+	QuickStart\Tools::simple_shortcode( $atts, $content, $tag  );
+}
+
+// =========================
+// !Hooks aliases
+// =========================
+
+/**
+ * Alias to the fix_shortcodes hook
+ *
+ * @see QuickStart\Hooks::_fix_shortcodes()
+ */
+function qs_fix_shortcodes( $tags ) {
+	QuickStart\Hooks::fix_shortcodes( $tags );
+}
+
+/**
+ * Alias to the disable_quickedit hook
+ *
+ * @see QuickStart\Hooks::_disable_quickedit()
+ */
+function qs_disable_quickedit( $post_types ) {
+	QuickStart\Hooks::disable_quickedit( $post_types );
+}
+
+/**
+ * Alias to the edit_meta_box hook
+ *
+ * @see QuickStart\Hooks::_edit_meta_box()
+ */
+function qs_edit_meta_box( $meta_box, $changes, $post_types = null ) {
+	QuickStart\Hooks::edit_meta_box( $meta_box, $changes, $post_types );
+}
