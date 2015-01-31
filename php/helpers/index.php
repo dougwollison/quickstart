cbbf4070e98a63e9cbf6d74c0a24555df46792b2
Doug Wollison <doug@wollison.net>
2015-01-31 18:50:45 -0500

    Added is_index_page function

--- php/helpers/index.php
+++ php/helpers/index.php
@@ -70,3 +70,62 @@ function the_index() {
 	$post = get_index( null, 'object' );
 	setup_postdata( $post );
 }
+
+/**
+ * Check if the a post is an index page.
+ *
+ * If you don't pass a post type, it'll return the slug of the post
+ * type it's the index page for. Otherwise, it'll return true/false.
+ *
+ * @since 1.8.0
+ *
+ * @param int|object $post_id          Optional The ID of the post to check.
+ * @param string     $$match_post_type Optional The post type to match.
+ *
+ * @return string|bool The result of the test.
+ */
+function is_index_page( $post_id = null, $match_post_type = null ) {
+	// Handle no post or post object, also get the post type
+	if ( is_null( $post_id ) ) {
+		global $post;
+		$post_type = $post->post_type;
+		$post_id = $post->ID;
+	} elseif ( is_object( $post_id ) ) {
+		$post_type = $post_id->post_type;
+		$post_id = $post_id->ID;
+	} else {
+		$post_type = get_post_type( $post_id );
+	}
+	
+	// Automatically return false if not a page
+	if ( $post_type != 'page' ) {
+		return false;
+	}
+	
+	// Reverse lookup the option name that this post's ID is the value of,
+	// provided it's a page_for_ option.
+	$option = $wpdb->get_var( $wpdb->prepare( "
+		SELECT option_name FROM $wpdb->options WHERE option_value = %d AND
+		(  option_name = 'page_for_posts' OR option_name LIKE 'page\_for\_%%\_posts' )
+		ORDER BY option_id DESC
+	", $post->ID ) );
+	
+	// Set the properties for testing and details if a match
+	if ( $option ) {
+		// Extract the post_type slug from the option name (default is post)
+		$for_post_type = 'post';
+		if ( preg_match( '/page_for_(.+)_posts/', $option, $matches ) ) {
+			$for_post_type = $matches[1];
+		}
+		
+		if ( is_null( $match_post_type ) ) {
+			// No match requested, return the post type
+			return $for_post_type;
+		} else {
+			// Match test requested, return result
+			return $match_post_type == $for_post_type;
+		}
+	}
+	
+	return false;
+}
