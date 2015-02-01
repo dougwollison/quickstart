2a11365e4fb39bdf2e34837d5c0f49475a7b5134
Doug Wollison <doug@wollison.net>
2015-02-01 00:08:26 -0500

    Added shit tone of head related template helpers, plus the_head to do everything in one go.

--- php/quickstart/class-template.php
+++ php/quickstart/class-template.php
@@ -40,14 +40,155 @@ class Template {
 	}
 
 	/**
+	 * Print out the viewport meta tag.
+	 *
+	 * @since 1.8.0
+	 *
+	 * @param array $settings Optional An array of settings to add/overwrite.
+	 */
+	public static function viewport( $settings = array() ){
+		// Handle the settings to go in the content attribue
+		$settings = wp_parse_args( $settings, array(
+			'width' => 'device-width',
+			'initial-scale' => 1,
+		) );
+
+		$content = array();
+		foreach ( $settings as $key => $value ) {
+			// Skip empty values
+			if ( is_null( $value ) ) {
+				continue;
+			}
+
+			// Add the pair
+			$content[] = "$key=$value";
+		}
+
+		echo '<meta name="viewport" content="'.implode(',', $content).'">';
+	}
+
+	/**
+	 * Print out the favicon link (along with apple icons if passed).
+	 *
+	 * @since 1.8.0
+	 *
+	 * @param string|array $settings Optional The favicon URL or array of favicons.
+	 */
+	public static function favicon( $settings = null ) {
+		$icon_url = home_url('/favicon.ico');
+		$settings = (array) $settings;
+
+		// Overwrite if needed; either 0th or favicon entry
+		if ( isset( $settings['favicon'] ) ) {
+			$icon_url = $settings['favicon'];
+		} elseif ( isset( $settings[0] ) ) {
+			$icon_url = $settings[0];
+		}
+
+		// Print the favicon, getting the mimetype from the extension
+		$ext = pathinfo( $icon_url, PATHINFO_EXTENSION );
+		$mimetype = $ext == 'ico' ? 'icon' : $ext;
+		echo '<link rel="shortcut icon" type="image/' . $mimetype . '" href="' . $icon_url . '" />';
+		echo "\n";
+
+		// Handle apple-touch icons if present
+		if ( isset( $settings['apple-touch'] ) ) {
+			$touch_icons = (array) $settings['apple-touch'];
+			$rel = 'apple-touch-icon-precomposed';
+
+			foreach ( $touch_icons as $size => $url ) {
+				if ( is_int( $size ) ) {
+					$size = '60x60';
+				}
+
+				echo '<link rel="' . $rel .'" sizes="' . $size . '" href="' . $url . '" />';
+				echo "\n";
+			}
+		}
+	}
+
+	/**
+	 * Print out the IE stylesheet.
+	 *
+	 * Pass a string for the URL (default is css/ie.css in the theme folder)
+	 * Pass an int for the IE version cap (default to 9)
+	 *
+	 * @since 1.8.0
+	 *
+	 * @param string|int|array $settings Optional The stylesheet URL and/or version number
+	 */
+	public static function ie_css( $settings = null ) {
+		$version = 9;
+		$css_url = THEME_URL . '/css/ie.css';
+
+		// Handle settings for custom url and/or version
+		if ( is_array( $settings ) ) {
+			if ( isset( $settings['version'] ) ) {
+				$version = $settings['version'];
+			}
+			if ( isset( $settings['src'] ) ) {
+				$css_url = $settings['src'];
+			}
+		} elseif ( is_int( $settings ) ) {
+			$version = $settings;
+		} elseif ( ! empty( $settings ) ) {
+			$css_url = $settings;
+		}
+
+		echo '<!--[if lt IE ' . $version . ']><link rel="stylesheet" type="text/css" href="' . $css_url . '" /><![endif]-->';
+	}
+
+	/**
+	 * Print out the HTML5 shiv, either a provided one or the google one.
+	 *
+	 * @since 1.8.0
+	 *
+	 * @param string $shiv_url Optional The URL to the shiv file (defaults to google one)
+	 */
+	public static function html5shiv( $shiv_url = null ){
+		if ( empty( $shiv_url ) ) {
+			$shiv_url = '//html5shiv.googlecode.com/svn/trunk/html5.js';
+		}
+		// Print out within an IE conditional comment
+		echo '<!--[if lt IE 9]><script src="' . $shiv_url . '"></script><![endif]-->';
+	}
+
+	/**
+	 * Print out the WP AJAX url for javascript.
+	 *
+	 * @since 1.8.0
+	 */
+	public static function ajaxurl(){
+		echo '<script>var ajaxurl = "' . admin_url('admin-ajax.php') . '";</script>';
+	}
+
+	/**
+	 * Print out the template url for javascript.
+	 *
+	 * @since 1.8.0
+	 */
+	public static function template_url(){
+		echo '<script>var template_url = "' . get_bloginfo('template_url') . '";</script>';
+	}
+
+	/**
 	 * Print out the Google Analytics gode.
 	 *
+	 * @since 1.8.0 Added ability to pass $account & $production as array for first argument.
 	 * @since 1.6.2
 	 *
-	 * @param string $account    The ID code of the account to track for.
-	 * @param string $production Optional A host name or IP address to check for before printing.
+	 * @param string|array $account    The ID code of the account to track for, or array of $account and $production.
+	 * @param string       $production Optional A host name or IP address to check for before printing.
 	 */
 	public static function ga_code( $account, $production = null ) {
+		// If $account is an array, split it into $account and $production
+		if ( is_array( $account ) ) {
+			if ( isset( $account[1] ) ) {
+				$production = $account[1];
+			}
+			$account = $account[0];
+		}
+
 		if ( ! is_null( $production ) ) {
 			// By default, check for the host name
 			$field = 'SERVER_NAME';
@@ -80,5 +221,50 @@ class Template {
 		</script>
 		<!-- End Google Analytics tracking code -->
 		<?php
+	}
+
+	/**
+	 * Print out the start of the header (doctype and head tag)
+	 *
+	 * This basically merges all the above template functions into one call.
+	 * By default only favicon is called, all others must be registered via $features.
+	 *
+	 * @since 1.8.0
+	 *
+	 * @param array $features An array of features to call.
+	 */
+	public static function the_head( array $features = array() ){
+		// Ensure the title-tag feature is supported
+		add_theme_support( 'title-tag' );
+
+		// Make sure favicon feature is set
+		if ( ! isset( $features['favicon'] ) && ! in_array( 'favicon', $features ) ) {
+			$features[] = 'favicon';
+		}
+
+		// Begin output
+		static::doc_start();
+		?>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=EDGE">
+
+	<?php wp_head(); ?>
+
+	<?php
+	// Call each feature method
+	foreach ( $features as $method => $settings ) {
+		if ( is_int( $method ) ) {
+			$method = $settings;
+			$settings = null;
+		}
+
+		call_user_func( "static::$method", $settings );
+		echo "\n";
+	}
+	?>
+</head>
+		<?php
+		// End output
 	}
 }
