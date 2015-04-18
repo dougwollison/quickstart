3f055a34052bdbbb2d6b1a754247527c927e192d
Doug Wollison <doug@wollison.net>
2015-04-18 15:43:05 -0400

    Added term meta helper.

---
+++ php/helpers/term_meta.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * Term Meta helpers; support setup and utilities for term meta data.
+ *
+ * @package QuickStart
+ * @subpackage Term_Meta
+ * @since 1.10.0
+ */
+ 
+// Register the new termmeta table
+global $wpdb;
+$wpdb->tables[] = 'termmeta';
+$wpdb->termmeta = $wpdb->prefix . 'termmeta';
+
+// Version number for update purposes
+define( 'QS_TERMMETA_VERSION', '1.0' );
+
+/**
+ * Register the termmeta table in the database.
+ *
+ * @since 1.10.0
+ */
+function qs_helper_termmeta_installtable() {
+	global $wpdb;
+	
+	// Skip if the version number is up to date and logged
+	if ( get_option( 'qs_termmeta_version' ) === QS_TERMMETA_VERSION ) {
+		return;
+	}
+
+	$charset_collate = '';
+
+	if ( ! empty($wpdb->charset) ) {
+		$charset_collate = "DEFAULT CHARACTER SET $wpdb->charset";
+	}
+	if ( ! empty($wpdb->collate) ) {
+		$charset_collate .= " COLLATE $wpdb->collate";
+	}
+
+	$sql = "CREATE TABLE $wpdb->termmeta (
+	  meta_id bigint(20) unsigned NOT NULL auto_increment,
+	  term_id bigint(20) unsigned NOT NULL default '0',
+	  meta_key varchar(255) default NULL,
+	  meta_value longtext,
+	  PRIMARY KEY  (meta_id),
+	  KEY comment_id (term_id),
+	  KEY meta_key (meta_key)
+	) $charset_collate;";
+
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	dbDelta( $sql );
+	
+	// Log the version number for updating and checking
+	update_option( 'qs_termmeta_version', QS_TERMMETA_VERSION );
+}
+add_action( 'plugins_loaded', 'qs_helper_termmeta_installtable' );
+
+/**
+ * Deletes all meta data tied to the term being deleted.
+ *
+ * @since 1.10.0
+ *
+ * @param int $term_id The ID of the term being deleted.
+ */
+function qs_helper_termmeta_deleteterm( $term_id ) {
+	global $wpdb;
+	
+	$wpdb->delete( $wpdb->termmeta, array(
+		'term_id' => $term_id,
+	) );
+}
+add_action( 'delete_term', 'qs_helper_termmeta_deleteterm' );
+
+/**
+ * Add meta data field to a term.
+ *
+ * @param int $term_id Term ID.
+ * @param string $meta_key Metadata name.
+ * @param mixed $meta_value Metadata value.
+ * @param bool $unique Optional, default is false. Whether the same key should not be added.
+ * @return int|bool Meta ID on success, false on failure.
+ */
+function add_term_meta( $term_id, $meta_key, $meta_value, $unique = false ) {
+	return add_metadata( 'term', $term_id, $meta_key, $meta_value, $unique );
+}
+
+/**
+ * Remove metadata matching criteria from a term.
+ *
+ * You can match based on the key, or key and value. Removing based on key and
+ * value, will keep from removing duplicate metadata with the same key. It also
+ * allows removing all metadata matching key, if needed.
+ *
+ * @param int $term_id term ID
+ * @param string $meta_key Metadata name.
+ * @param mixed $meta_value Optional. Metadata value.
+ * @return bool True on success, false on failure.
+ */
+function delete_term_meta( $term_id, $meta_key, $meta_value = '' ) {
+	return delete_metadata( 'term', $term_id, $meta_key, $meta_value );
+}
+
+/**
+ * Retrieve term meta field for a term.
+ *
+ * @param int $term_id Term ID.
+ * @param string $key Optional. The meta key to retrieve. By default, returns data for all keys.
+ * @param bool $single Whether to return a single value.
+ * @return mixed Will be an array if $single is false. Will be value of meta data field if $single
+ *  is true.
+ */
+function get_term_meta( $term_id, $key = '', $single = false ) {
+	return get_metadata( 'term', $term_id, $key, $single );
+}
+
+/**
+ * Update term meta field based on term ID.
+ *
+ * Use the $prev_value parameter to differentiate between meta fields with the
+ * same key and term ID.
+ *
+ * If the meta field for the term does not exist, it will be added.
+ *
+ * @param int $term_id Term ID.
+ * @param string $meta_key Metadata key.
+ * @param mixed $meta_value Metadata value.
+ * @param mixed $prev_value Optional. Previous value to check before removing.
+ * @return int|bool Meta ID if the key didn't exist, true on successful update, false on failure.
+ */
+function update_term_meta( $term_id, $meta_key, $meta_value, $prev_value = '' ) {
+	return update_metadata( 'term', $term_id, $meta_key, $meta_value, $prev_value );
+}
