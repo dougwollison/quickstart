b5b286422f1222872f3e5333b48488824d5f9c4e
Doug Wollison <doug@wollison.net>
2016-06-29 11:00:07 -0400

    tag typo

--- inc/quickstart/class-setup.php
+++ inc/quickstart/class-setup.php
@@ -1208,7 +1208,7 @@ class Setup extends \Smart_Plugin {
 	 * @since 1.12.0 Updated to use external handle_shorthand(),
 	 *               Also made sure fields arg was array before passing through maybe_load_media_manager()
 	 *               or looping through to register meta fields.
-	 * @sicne 1.11.1 Made sure fields arg was an array before passing through handle_shorthand().
+	 * @since 1.11.1 Made sure fields arg was an array before passing through handle_shorthand().
 	 * @since 1.11.0 Added use of static::handle_shorthand().
 	 *               Now public again.
 	 * @since 1.9.0  Now protected.
