d791c762e1766a36b8fedc25e61ff0e1940fa5af
Doug Wollison <doug@wollison.net>
2020-07-14 10:02:54 -0400

    restructure as mu-plugin
    will require a quickstart.php file in root of mu-plugins that loads load.php
    
    also drop constants file

--- load.php
+++ load.php
@@ -1,14 +1,4 @@
 <?php
-/*
-Plugin Name: QuickStart
-Plugin URI: https://github.com/dougwollison/quickstart
-Description: A utility kit for quick development of WordPress themes (and plugins). <strong>YOUR CUSTOM THEME RELIES ON THIS PLUGIN</strong>
-Version: 1.13.0
-Author: Doug Wollison
-Author URI: http://dougw.me
-License: GPL2
-*/
-
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
 	die;
@@ -26,6 +16,5 @@ define( 'QS_DIR', __DIR__ );
 require ( QS_DIR . '/inc/utilities.php' );            // Publicly accessible utilities
 require ( QS_DIR . '/inc/autoloader.php' );           // Class autoloaders
 require ( QS_DIR . '/inc/aliases.php' );              // Aliases for easier external access
-require ( QS_DIR . '/inc/constants.php' );            // Handy constants for paths/urls
 require ( QS_DIR . '/inc/hooks.php' );                // Publicly accessible hooks
 require ( QS_DIR . '/inc/quickstart/functions.php' ); // Internal-use utilities
